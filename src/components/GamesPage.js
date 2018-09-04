f01522a6be031d45e8a67d5ebc90602b6d663462
Haoran Feng <harrifeng@gmail.com>
2018-09-04 17:22:01 +0800

    ok

--- src/components/GamesPage.js
+++ src/components/GamesPage.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Games List from './GamesList';
 
 class GamesPage extends Component {
   render() {
     return (
       <div>
-        Games
+        <GamesList games={ this.props.games } />
       </div>
     );
   }
 }
 
-export default GamesPage;
+GamesPage.propTypes = {
+  games: PropTypes.array.isReuired
+}
+
+const mapStateToProps = (state) => {
+  return {
+    games: state.games
+  };
+};
+
+export default connect(mapStateToProps)(GamesPage);
