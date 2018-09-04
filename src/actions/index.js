23364bf4874aa61bb2c36f659d1af6cc02e23008
Haoran Feng <harrifeng@gmail.com>
2018-09-04 20:48:27 +0800

    go on

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,14 @@
+export const setGames = (games) => {
+  return {
+    type: 'SET_GAMEs',
+    games
+  }
+};
+
 export const fetchGames = () => {
   return dispatch => {
     fetch('/api/games')
+      .then(res => res.json())
+      .then(data => dispatch(setGames(data.games)))
   }
 }
