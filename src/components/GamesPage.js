9ac2b7a441209ae1de9159157ed751cfa5b65163
Haoran Feng <harrifeng@gmail.com>
2018-09-04 17:25:02 +0800

    fix bug

--- src/components/GamesPage.js
+++ src/components/GamesPage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import Games List from './GamesList';
+import GamesList from './GamesList';
 
 class GamesPage extends Component {
   render() {
